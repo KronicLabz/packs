e903706e9e31b3e48d38cebfcce8309db6ea84a6
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-10-25 20:17:07 -0500

    Update deployPack.mjs

--- scripts/deployPack.mjs
+++ scripts/deployPack.mjs
@@ -7,7 +7,7 @@ dotenv.config();
 
   const packAddress = await sdk.deployer.deployPack({
     name: "Treasure Chests",
-    primary_sale_recipient: "0xb371d1C5629C70ACd726B20a045D197c256E1054",
+    primary_sale_recipient: "0x366159340Ee4Cf5F4475cdDF0E17ED525B2d8F7C",
   });
 
   console.log(`Pack address: ${packAddress}`);
