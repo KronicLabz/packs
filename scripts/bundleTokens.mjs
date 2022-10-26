aa54d15575e6b528a47d16e5a210cba758bf9e41
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-10-25 20:16:12 -0500

    Update bundleTokens.mjs

--- scripts/bundleTokens.mjs
+++ scripts/bundleTokens.mjs
@@ -1,119 +1,57 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
 import dotenv from "dotenv";
 dotenv.config();
+
 import fs from "fs";
 
 (async () => {
-  const packAddress = "0x0Aee160411473f63be2DfF2865E81A1D59636C97";
-  const tokenAddress = "0x270d0f9DA22332F33159337E3DE244113a1C863C";
-  const editionAddress = "0xb4A48c837aB7D0e5C85eA2b0D9Aa11537340Fa17";
+    const packAddress = "0x1D7714b0c712D601b6058B566e0450a2275594EE";
+    const tokenAddress = "0x2c920d603C3d5c8f6d6E8de39511781D2b12aF74";
+    const editionAddress = "0x19f5d531C0595597ce7271d1F6C0F97B9355fBac";
 
-  const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "mumbai");
+    const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "goerli");
 
-  const pack = sdk.getPack(packAddress);
+    const pack = sdk.getContract;("0x5F245313B874f4F5c35136E9c6Bc630Bc6C9c484");
 
-  // Set approval for the pack contract to act upon token and edition contracts
-  const token = sdk.getToken(tokenAddress);
-  await token.setAllowance(packAddress, 100);
+    const token = await sdk.getContract(tokenAddress);
+    await token.setApprovalForAll(packAddress);
+    await token.setAllowance(packAddress, true);
+    console.log("Token approved");
 
-  console.log("Set approval for token");
+    const edition = await sdk.getContract(editionAddress);
+    await edition.setApprovalForAll(packAddress, true);
+    console.log("Edition approved");
 
-  const edition = sdk.getEdition(editionAddress);
-  await edition.setApprovalForAll(packAddress, true);
+    const packFile = fs.readFileSync("./scripts/pack.png");
+    const url = `${ipfsHash,baseUri}`;
+    console.log("Pack file uploaded to IPFS");
 
-  console.log("Set Approval for edition");
+    console.log("Creating packs now...");
+    const packNfts = await pack.create({
+        packMetadata: {
+            name: "BrainzZz",
+            description: "BrainzZz",
+            image: url,
+        },
+        erc20Rewards: [
+            {
+                contractAddress: tokenAddress,
+                quantityPerReward: 1,
+                quantity: 45,
+                totalRewards: 45,
+            },
+        ],
+        erc1155Rewards: [
+            {
+                contractAddress: editionAddress,
+                quantityPerReward: 1,
+                quantity: 1,
+                totalRewards: 45,
+            },
+        ],
+        rewardsPerPack: 2,
+    });
 
-  // Read in the chest.png as a File using fs
-  const chestFile = fs.readFileSync("./scripts/chest.png");
-
-  // Upload the Chest to IPFS
-  const ipfsHash = await sdk.storage.upload(chestFile);
-  const url = ipfsHash.uris[0];
-
-  console.log("Uploaded chest asset to IPFS");
-
-  console.log("Creating packs now...");
-
-  const packNfts = await pack.create({
-    packMetadata: {
-      name: "Treasure Chest",
-      description:
-        "A chest containing tools and treasure to help you on your voyages.",
-      image: url,
-    },
-
-    // Gold coin ERC-20 Tokens
-    erc20Rewards: [
-      {
-        contractAddress: tokenAddress,
-        quantityPerReward: 5,
-        quantity: 100,
-        totalRewards: 20,
-      },
-    ],
-
-    erc1155Rewards: [
-      // Compass
-      {
-        contractAddress: editionAddress,
-        tokenId: 0,
-        quantityPerReward: 1,
-        totalRewards: 100,
-      },
-      // Anchor
-      {
-        contractAddress: editionAddress,
-        tokenId: 1,
-        quantityPerReward: 1,
-        totalRewards: 100,
-      },
-      // Sword
-      {
-        contractAddress: editionAddress,
-        tokenId: 2,
-        quantityPerReward: 1,
-        totalRewards: 100,
-      },
-      // Captain's Hat
-      {
-        contractAddress: editionAddress,
-        tokenId: 3,
-        quantityPerReward: 1,
-        totalRewards: 65,
-      },
-      // Cannon
-      {
-        contractAddress: editionAddress,
-        tokenId: 4,
-        quantityPerReward: 1,
-        totalRewards: 50,
-      },
-      // Hook
-      {
-        contractAddress: editionAddress,
-        tokenId: 5,
-        quantityPerReward: 1,
-        totalRewards: 50,
-      },
-      // Rum
-      {
-        contractAddress: editionAddress,
-        tokenId: 6,
-        quantityPerReward: 1,
-        totalRewards: 10,
-      },
-      // Gold Key
-      {
-        contractAddress: editionAddress,
-        tokenId: 7,
-        quantityPerReward: 1,
-        totalRewards: 5,
-      },
-    ],
-    rewardsPerPack: 5,
-  });
-
-  console.log(`====== Success: Pack NFTs =====`);
-
-  console.log(packNfts);
+    console.log("Packs created");
 })();
